6969dbba543177f73dcb213a96ebe2281a7fc5b8
David Ramírez <rrdavid08@gmail.com>
2024-01-27 21:17:55 +0100

    ADDED random button correct functionality and style for the food cards

--- src/js/byRandom.js
+++ src/js/byRandom.js
@@ -1,13 +1,36 @@
 const urlRecipes = "https://www.themealdb.com/api/json/v1/1/random.php";
 const recipesContainer = document.getElementById("recipes-container");
 
-export async function getByRandom() {
-   const response = await fetch(urlRecipes);
-   const randomRecipe = await response.json();
+function createCard(recipe) {
+   const template =
+   `
+   <div class="col-3">
+   <div class="card" style="width: 18rem;">
+      <img src="${recipe.meals[0].strMealThumb}" class="card-img-top" alt="Food photo">
+      <div class="card-body">
+         <h5 class="card-title">${recipe.meals[0].strCategory}</h5>
+         <p class="card-text">${recipe.meals[0].strMeal}</p>
+         <a href="#" class="btn btn-primary">Add to favourites</a>
+      </div>
+   </div>
+   </div>`;
+   
+   return template;
+}
 
-   recipesContainer.innerHTML =
-   `<ul>
-      <li>${randomRecipe.meals[0].strMeal}</li>
-      <img src="${randomRecipe.meals[0].strMealThumb}" alt="Food photo">
-   </ul>`;
+export async function getByRandom() {
+   const promises = [];
+   
+   for (let i = 0; i <= 3; i++) {
+      const response = await fetch(urlRecipes);
+      const recipe = await response.json();
+
+      promises.push(recipe);      
+   }
+
+   const randomRecipes = await Promise.all(promises);
+   randomRecipes.forEach(randomRecipe => {
+      recipesContainer.innerHTML += createCard(randomRecipe);
+   })
+   
 }
