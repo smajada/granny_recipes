1fd5b7c0d2dc6fc91847c033061e06f61608831f
David Ramírez <rrdavid08@gmail.com>
2024-02-03 12:05:37 +0100

    modified(modal): modal style modified to make it more clear

--- src/js/byRandom.js
+++ src/js/byRandom.js
@@ -23,44 +23,45 @@ function createCard(recipe) {
 }
 
 function createModal(recipe) {
-   const modalId = recipe.meals[0].idMeal;
-   const modalTitle = recipe.meals[0].strMeal;
-   const modalImage = recipe.meals[0].strMealThumb;
-   const modalInstructions = recipe.meals[0].strInstructions;
-   const modalCategory = recipe.meals[0].strCategory;
+   const recipeId = recipe.meals[0].idMeal;
+   const recipeTitle = recipe.meals[0].strMeal;
+   const recipeImage = recipe.meals[0].strMealThumb;
+   const recipeInstructions = recipe.meals[0].strInstructions;
+   const recipeCategory = recipe.meals[0].strCategory;
+   const recipeArea = recipe.meals[0].strArea;
    const ingredientsList = createIngredientsList(recipe.meals[0]);
    
    const template = `
-      <div class="modal fade" id="modal_${modalId}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal fade" id="modal_${recipeId}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
          <div class="modal-dialog modal-xl">
          <div class="modal-content">
             <div class="modal-header">
-               <h1 class="modal-title fs-5" id="exampleModalLabel">${modalTitle}</h1>
+               <h1 class="modal-title" id="exampleModalLabel">${recipeTitle} - <span class="text-secondary">${recipeCategory}</span></h1>
                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body d-flex">
-               <div>
-                  <div class="text-center">
-                     <h2 class="">${modalCategory}</h2>
-                  </div>
-                  <div>
-                     <h3 class="">Instructions</h3>
-                     <p class="">${modalInstructions}</p>
-                  </div>
-               </div>
+            <div class="modal-body d-flex flex-column">
 
-               <div>
-                  <img src="${modalImage}" class="h-450" alt="Food photo"/>
-                  <div>
-                     <div class="text-center">
-                        <h3 class="">Ingredients</h3>
-                     </div>
-                     <div>
-                        <ul class="mt-2 d-flex flex-wrap">
+               <div class="d-flex">
+                  <img src="${recipeImage}" class="h-400 rounded" alt="Food photo"/>
+
+                  <div class="h-max-400 d-flex flex-grow-1 justify-content-around align-items-start">
+                     <div class="">
+                        <h3 class="text-center">Ingredients</h3>
+                        <ul class="mt-2">
                            ${ingredientsList}
                         </ul>
                      </div>
+                     <div class="d-flex flex-column w-max-400">
+                        <h3 class="text-center">Info</h3>
+                        <p><span class="fw-bold">Area:</span> ${recipeArea}</p>
+                        <p><span class="fw-bold">Food categroy:</span> ${recipeCategory}</p>
+                     </div>
                   </div>
+               </div>
+
+               <div class="mt-3">
+                  <h3>Instructions</h3>
+                  <p class="text-justify">${recipeInstructions}</p>
                </div>
             </div>
             <div class="modal-footer">
