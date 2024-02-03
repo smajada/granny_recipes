81b94166f68a035b7fc745e7978ff5b61ac3bd0a
David Ramírez <rrdavid08@gmail.com>
2024-02-03 01:57:49 +0100

    feat(modal): added ingredients and measures to the modal for every recipe

--- src/js/byRandom.js
+++ src/js/byRandom.js
@@ -2,6 +2,7 @@ const urlRecipes = "https://www.themealdb.com/api/json/v1/1/random.php";
 const recipesContainer = document.getElementById("recipes-container");
 const modalContainer = document.getElementById("modal-container");
 
+
 function createCard(recipe) {
    const template = `
    <div class="col-3">
@@ -22,21 +23,29 @@ function createCard(recipe) {
 }
 
 function createModal(recipe) {
+   const modalId = recipe.meals[0].idMeal;
+   const modalTitle = recipe.meals[0].strMeal;
+   const modalImage = recipe.meals[0].strMealThumb;
+   const modalInstructions = recipe.meals[0].strInstructions
+   const ingredientsList = createIngredientsList(recipe.meals[0]);
+   
    const template = `
-      <div class="modal fade" id="modal_${recipe.meals[0].idMeal}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal fade" id="modal_${modalId}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
          <div class="modal-dialog modal-xl">
          <div class="modal-content">
             <div class="modal-header">
-               <h1 class="modal-title fs-5" id="exampleModalLabel">${recipe.meals[0].strMeal}</h1>
+               <h1 class="modal-title fs-5" id="exampleModalLabel">${modalTitle}</h1>
                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
             <div d-flex flex-column>
+               <img src="${modalImage}" class="w-100 h-auto" alt="Food photo">
+               <h5 class="mt-3">Ingredients:</h5>
+               <ul class="mt-2">
+                  ${ingredientsList}
+               </ul>
                <div>
-                  <img src="${recipe.meals[0].strMealThumb}" alt="Food photo">
-               </div>
-               <div>
-                  ${recipe.meals[0].strInstructions}
+                  ${modalInstructions}
                </div>
             </div>
             </div>
@@ -49,6 +58,22 @@ function createModal(recipe) {
       </div>`;
    
    return template;
+}
+
+function createIngredientsList(meal) {
+
+   let ingredientsList = '';
+   for (let i = 1; i < 20; i++) {
+      const ingredient = meal[`strIngredient${i}`]
+      const measure = meal[`strMeasure${i}`]
+      
+      if (!ingredient && !measure) {
+         break;
+      }
+
+      ingredientsList += `<li>${ingredient} - ${measure}</li>`
+   }
+   return ingredientsList;
 }
 
 export async function getByRandom() {
