651b0abf72832e59c87dbbaa30e59ab4bee02cb7
Sergio Majada <smajadamanresa@cifpfbmoll.eu>
2024-02-05 11:31:12 +0100

    fix: deleted wrong listCategories func

--- src/js/byCategory.js
+++ src/js/byCategory.js
@@ -1,46 +1,46 @@
 import { addRecipeToIndexedDB } from "./indexedDB";
-import { createCard, createModal, createCategory } from './createElements';
-import { getById } from './byId';
+import { createCard, createModal, createCategory } from "./createElements";
+import { getById } from "./byId";
 
 const urlCategories = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
 const urlRecipe = "https://www.themealdb.com/api/json/v1/1/filter.php";
 const categoriesContainer = document.getElementById("categories-container");
 const recipesContainer = document.getElementById("recipes-container");
 const modalContainer = document.getElementById("modal-container");
-const spinnerLoader = document.getElementById('spinner-loader');
+const spinnerLoader = document.getElementById("spinner-loader");
 
 /**
  * Fetch de recetas por categoría.
- * 
+ *
  * Obtiene y muestra recetas de una categoría específica.
  * @param {string} category - Categoría de la que se obtendrán las recetas.
  * @returns {Promise<void>} - Promesa que resuelve cuando se han obtenido y mostrado las recetas.
  */
 export async function getByCategory(category) {
-   const showMoreBtn = document.getElementById('showMore-btn');
-   const categoryTitle = document.getElementById('category-title');
+	const showMoreBtn = document.getElementById("showMore-btn");
+	const categoryTitle = document.getElementById("category-title");
 
-   // Realiza una solicitud a la API para obtener recetas de la categoría especificada
-   const promise = await fetch(`${urlRecipe}?c=${category}`);
-   const recipes = await promise.json();
+	// Realiza una solicitud a la API para obtener recetas de la categoría especificada
+	const promise = await fetch(`${urlRecipe}?c=${category}`);
+	const recipes = await promise.json();
 
-   // Limpia los contenedores, actualiza el título de la categoría y esconde el spinner de carga
-   recipesContainer.innerHTML = '';
-   modalContainer.innerHTML = '';
-   categoryTitle.innerHTML = category;
-   spinnerLoader.classList.remove('d-flex');
-   spinnerLoader.classList.add('d-none');
+	// Limpia los contenedores, actualiza el título de la categoría y esconde el spinner de carga
+	recipesContainer.innerHTML = "";
+	modalContainer.innerHTML = "";
+	categoryTitle.innerHTML = category;
+	spinnerLoader.classList.remove("d-flex");
+	spinnerLoader.classList.add("d-none");
 
-   // Itera sobre las recetas obtenidas y las muestra en la página
-   recipes.meals.forEach(async (recipe) => {
-      // Obtiene detalles adicionales de la receta por su ID
-      const recipeById = await getById(recipe.idMeal);
-      showMoreBtn.style.display = 'none';
+	// Itera sobre las recetas obtenidas y las muestra en la página
+	recipes.meals.forEach(async (recipe) => {
+		// Obtiene detalles adicionales de la receta por su ID
+		const recipeById = await getById(recipe.idMeal);
+		showMoreBtn.style.display = "none";
 
-      // Agrega las tarjetas y modales al HTML
-      recipesContainer.innerHTML += createCard(recipeById);
-      modalContainer.innerHTML += createModal(recipeById);
-   });
+		// Agrega las tarjetas y modales al HTML
+		recipesContainer.innerHTML += createCard(recipeById);
+		modalContainer.innerHTML += createModal(recipeById);
+	});
 	// Add event listener to heart buttons
 	recipesContainer.addEventListener("click", (event) => {
 		const target = event.target;
@@ -54,27 +54,20 @@ export async function getByCategory(category) {
 		}
 	});
 }
-
-export async function listCategories() {
-	if (categoriesContainer !== null) {
-		const promise = await fetch(urlCategories);
-		const categories = await promise.json();
-
-
 /**
  * Fetch de las categorías.
- * 
+ *
  * Obtiene y lista las categorías de comidas disponibles.
  * @returns {Promise<void>} - Promesa que resuelve cuando se han obtenido y mostrado las categorías.
  */
 export async function listCategories() {
-   // Verifica la existencia del contenedor de categorías
-   if (categoriesContainer !== null) {
-      // Realiza una solicitud a la API para obtener la lista de categorías
-      const promise = await fetch(urlCategories);
-      const categories = await promise.json();
+	// Verifica la existencia del contenedor de categorías
+	if (categoriesContainer !== null) {
+		// Realiza una solicitud a la API para obtener la lista de categorías
+		const promise = await fetch(urlCategories);
+		const categories = await promise.json();
 
-      // Agrega el HTML de las categorías al contenedor
-      categoriesContainer.innerHTML = createCategory(categories);
-   }
+		// Agrega el HTML de las categorías al contenedor
+		categoriesContainer.innerHTML = createCategory(categories);
+	}
 }
